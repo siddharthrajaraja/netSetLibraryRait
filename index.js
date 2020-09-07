429720734b8df4c01e4d797e96dbe6e7f27e8d86
SIDDHARTH RAMESH CHAND RAJA <siddharthraja9849@gmail.com>
2020-09-07 12:00:21 +0530

    adding homepage

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express')
+
+var app = new express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('homepage.ejs')
+})
+
+app.get('/error', (req, res) => {
+    res.send(JSON.stringify('NO URL FOUND!!'))
+})
+
+app.use((req, res, next) => {
+    res.redirect('/error')
+})
+
+app.listen(8000, () => {
+    console.log("Listening to 8000 PORT")
+})
